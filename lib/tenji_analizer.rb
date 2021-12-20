c5cb33ec6ce26204d5b8630c642a3b02e461dd9d
4geru <westhouse51@gmail.com>
2021-12-21 01:46:17 +0900

    add TenjiAnalizer.word_detector

--- lib/tenji_analizer.rb
+++ lib/tenji_analizer.rb
@@ -11,6 +11,30 @@ class TenjiAnalizer
     まみむめも
     らりるれろ
   ]
+  # あ：1
+  # い：1, 2
+  # う：1, 4
+  # え：1, 2, 4
+  # お：2, 4
+  BOIN = [1, 3, 5, 11, 10]
+  # あ：なし
+  # か：6
+  # さ：5, 6
+  # た：3, 5
+  # な：3
+  # は：3, 6
+  # ま：3, 5, 6
+  # ら：5
+  SHIIN = [0, 32, 48, 20, 4, 36, 52, 16]
+
+  def word_detector(word)
+    shiin, boin = word_position_detector(word)
+    word_binary_detector(boin, shiin)
+  end
+
+  def word_binary_detector(boin, shiin)
+    BOIN[boin] | SHIIN[shiin]
+  end
 
   def word_position_detector(word)
     TEMPLATE_JAPANESE_MAP.each_with_index do |line, index|
--- spec/tenji_analizer_spec.rb
+++ spec/tenji_analizer_spec.rb
@@ -2,6 +2,22 @@ require "spec_helper"
 require "./lib/tenji_analizer"
 
 describe "TenjiAnalizer" do
+  describe '.word_detector' do
+    subject { TenjiAnalizer.new.word_detector(word) }
+    # context 'あのとき' do
+    #   let(:word) { 'あ' }
+    #   it "1が返ってくる" do
+    #     expect(subject).to eq(1)
+    #   end
+    # end
+    fcontext 'さのとき' do
+      let(:word) { 'さ' }
+      it "49が返ってくる" do
+        expect(subject).to eq(49)
+      end
+    end
+  end
+
   describe '.word_position_detector' do
     subject { TenjiAnalizer.new.word_position_detector(word) }
     context 'あのとき' do
